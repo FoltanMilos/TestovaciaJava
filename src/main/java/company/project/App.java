72d031115a89a2538f807c79497c15be86fb69c2
Milos Foltan <foltan.milosml@gmail.com>
2020-05-31 12:42:27 +0200

    initialss

---
+++ src/main/java/company/project/App.java
@@ -0,0 +1,14 @@
+package company.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //zzzz
+    }
+}
